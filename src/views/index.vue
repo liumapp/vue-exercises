723afced80bce4ce575f1e87ff84a014bb2b91d8
liumapp <liumapp.com@gmail.com>
2018-07-19 11:37:51 +0800

    add iview dependencies

---
+++ src/views/index.vue
@@ -0,0 +1,15 @@
+/**
+ * @file index.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/19/18
+ */
+<template>
+  
+</template>
+<script>
+export default {
+  name: 'index'
+}
+</script>
